6e8709be9eb9627bbd0ddb1646e0737ea3dd48d4
qiweikai <304333748@qq.com>
2024-03-30 12:27:32 +0800

    feat: enable function component

--- App.jsx
+++ App.jsx
@@ -1,6 +1,24 @@
-import React from './core/React.js';
-const App = React.createElement("div", {
-  id: "app"
-}, "hi- ", "mini-react");
+import React from "./core/React.js";
+// const App = React.createElement("div", {
+//   id: "app"
+// }, "hi- ", "mini-react");
+
+function Counter({ num }) {
+  return <div>count: {num}</div>;
+}
+
+function CounterContainer() {
+  return <Counter num={20}></Counter>;
+}
+
+const App = function () {
+  return (
+    <div>
+      hi-mini-react
+      <Counter num={10}></Counter>
+      <CounterContainer></CounterContainer>
+    </div>
+  );
+};
 
 export default App;
--- core/React.js
+++ core/React.js
@@ -40,7 +40,8 @@ function createElement(type, props, ...children) {
     props: {
       ...props,
       children: children.map((child) => {
-        return typeof child === 'string' ? createTextNode(child) : child;
+        const isTextNode = typeof child === 'string' || typeof child === 'number'
+        return isTextNode ? createTextNode(child) : child;
       })
     }
   }
@@ -74,7 +75,13 @@ function commitRoot() {
 
 function commitWork(fiber) {
   if (!fiber) return;
-  fiber.parent.dom.append(fiber.dom);
+  let fiberParent = fiber.parent;
+  while (!fiberParent.dom) {
+    fiberParent = fiberParent.parent;
+  }
+  if (fiber.dom) {
+    fiberParent.dom.append(fiber.dom);
+  }
   commitWork(fiber.child);
   commitWork(fiber.sibling);
 }
@@ -92,8 +99,7 @@ function updateProps(dom, props) {
   })
 }
 
-function initChildren(fiber) {
-  const children = fiber.props.children;
+function initChildren(fiber, children) {
   let prevChild = null;
   children.forEach((child, index) => {
     const newFiber = {
@@ -113,19 +119,31 @@ function initChildren(fiber) {
   })
 }
 
-function performWorkOfUnit(fiber) {
+function updateFunctionComponent(fiber) {
+  const children = [fiber.type(fiber.props)]
+  initChildren(fiber, children);
+}
+
+function updateHostComponent(fiber) {
   //! 创建dom
   if (!fiber.dom) {
     const dom = (fiber.dom = createDom(fiber.type));
-
-    // fiber.parent.dom.append(dom);
-
     //! 处理 props
     updateProps(dom, fiber.props)
   }
+  const children = fiber.props.children;
 
   //! 转换成链表
-  initChildren(fiber);
+  initChildren(fiber, children);
+}
+
+function performWorkOfUnit(fiber) {
+  const isFunctionComponent = typeof fiber.type === 'function';
+  if (isFunctionComponent) {
+    updateFunctionComponent(fiber)
+  } else {
+    updateHostComponent(fiber);
+  }
 
   //! 返回下一个要执行的任务
   if (fiber.child) {
@@ -135,7 +153,13 @@ function performWorkOfUnit(fiber) {
   if (fiber.sibling) {
     return fiber.sibling;
   }
-  return fiber.parent.sibling;
+  // 循环向上查找
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) return nextFiber.sibling;
+    nextFiber = nextFiber.parent;
+  }
+  // return fiber.parent.sibling;
 }
 
 requestIdleCallback(workLoop);
